f5a02f0d5e276f7f902d3e2103bb227641c894fb
shaurya <shauryaverma4296@gmail.com>
2021-02-03 13:46:41 +0530

    Adding mongodb container

--- backend_node/app.js
+++ backend_node/app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 var port = 8000;
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -11,28 +11,29 @@ const connection = "mongodb://mongo:27017/node-demo";
 //mongoose.connect("mongodb://localhost:27017/node-demo");
 mongoose.connect(connection);
 var nameSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String
+  firstName: String,
+  lastName: String,
 });
 var User = mongoose.model("User", nameSchema);
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/addname", (req, res) => {
-    var myData = new User(req.body);
-    myData.save()
-        .then(item => {
-            res.send("Name saved to database");
-        })
-        .catch(err => {
-            res.status(400).send("Unable to save to database");
-        });
+  var myData = new User(req.body);
+  myData
+    .save()
+    .then((item) => {
+      res.send("Name saved to database");
+    })
+    .catch((err) => {
+      res.status(400).send("Unable to save to database");
+    });
 });
 
 app.listen(port, () => {
-    console.log("Server listening on port " + port);
+  console.log("Server listening on port " + port);
 });
 
-module.exports = app
+module.exports = app;
